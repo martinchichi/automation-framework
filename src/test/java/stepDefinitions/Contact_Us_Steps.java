c91c18a703396c9f24241f3a1400d804047584aa
martinchichi <martincicinelli@gmail.com>
2023-08-27 12:37:01 -0300

    Implementing Step Definitions

--- src/test/java/stepDefinitions/Contact_Us_Steps.java
+++ src/test/java/stepDefinitions/Contact_Us_Steps.java
@@ -7,13 +7,18 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 
 public class Contact_Us_Steps {
     private WebDriver driver;
+    String contactUsUrl = "https://www.webdriveruniversity.com/Contact-Us/contactus.html";
 
 
     @Before
@@ -30,38 +35,49 @@ public class Contact_Us_Steps {
         driver.quit();
     }
 
+    public String generateRandomNumber(int length){
+        return RandomStringUtils.randomNumeric(length);
+    }
+    public String generateRandomString(int length){
+        return RandomStringUtils.randomAlphabetic(length);
+    }
+
     @Given("I access the webdriver university contact us page")
     public void i_access_the_webdriver_university_contact_us_page() {
-        System.out.println("Test1");
+        driver.get(contactUsUrl);
     }
 
     @When("I enter a unique first name")
     public void i_enter_a_unique_first_name() {
-        System.out.println("Test2");
+        driver.findElement(By.xpath("//input[@name='first_name']")).sendKeys("AutoFN" + generateRandomNumber(5));
     }
 
     @And("I enter a unique last name")
     public void i_enter_a_unique_last_name() {
-        System.out.println("Test3");
+        driver.findElement(By.xpath("//input[@name='last_name']")).sendKeys("AutoLN" + generateRandomNumber(5));
     }
 
     @And("I enter a unique email address")
     public void i_enter_a_unique_email_address() {
-        System.out.println("Test4");
+        driver.findElement(By.xpath("//input[@name='email']")).sendKeys("AutoEmail" + generateRandomNumber(5) + "@mail.com");
+
     }
 
     @And("I enter a unique comment")
     public void i_enter_a_unique_comment() {
-        System.out.println("Test5");
+        driver.findElement(By.xpath("//textarea[@name='message']")).sendKeys("Test" + generateRandomString(10));
     }
 
     @And("I click on the submit button")
     public void i_click_on_the_submit_button() {
-        System.out.println("Test6");
+    driver.findElement(By.xpath("//input[@value='SUBMIT']")).click();
     }
 
     @Then("I should be presented with a successful contact us submission message")
     public void i_should_be_presented_with_a_successful_contact_us_submission_message() {
-        System.out.println("Test7");
+        WebElement contactUs_Submission_Message = driver.findElement(By.xpath("//div[@id='contact_reply']/h1"));
+        Assert.assertEquals(contactUs_Submission_Message.getText(), "Thank You for your Message!");
+
+
     }
 }
