51f95726fef874cec72d45c9ce8b53ac5b5fea56
martinchichi <martincicinelli@gmail.com>
2023-09-01 14:17:08 -0300

    Cucumber Expressions

--- src/test/java/stepDefinitions/Contact_Us_Steps.java
+++ src/test/java/stepDefinitions/Contact_Us_Steps.java
@@ -36,6 +36,7 @@ public class Contact_Us_Steps {
     }
 
     public String generateRandomNumber(int length){
+
         return RandomStringUtils.randomNumeric(length);
     }
     public String generateRandomString(int length){
@@ -60,12 +61,28 @@ public class Contact_Us_Steps {
     @And("I enter a unique email address")
     public void i_enter_a_unique_email_address() {
         driver.findElement(By.xpath("//input[@name='email']")).sendKeys("AutoEmail" + generateRandomNumber(5) + "@mail.com");
-
     }
 
     @And("I enter a unique comment")
     public void i_enter_a_unique_comment() {
         driver.findElement(By.xpath("//textarea[@name='message']")).sendKeys("Test" + generateRandomString(10));
+    }
+
+    @When("I enter a specific first name {word}")
+    public void i_enter_a_specific_first_name(String firstName) {
+        driver.findElement(By.xpath("//input[@name='first_name']")).sendKeys(firstName);
+    }
+    @And("I enter a specific last name {word}")
+    public void i_enter_a_specific_last_name(String lastName) {
+        driver.findElement(By.xpath("//input[@name='last_name']")).sendKeys(lastName);
+    }
+    @And("I enter a specific email address {word}")
+    public void i_enter_a_specific_email_address(String email) {
+        driver.findElement(By.xpath("//input[@name='email']")).sendKeys(email);
+    }
+    @And("I enter a specific comment {string}")
+    public void i_enter_a_specific_comment(String comment) {
+        driver.findElement(By.xpath("//textarea[@name='message']")).sendKeys(comment);
     }
 
     @And("I click on the submit button")
@@ -77,6 +94,8 @@ public class Contact_Us_Steps {
     public void i_should_be_presented_with_a_successful_contact_us_submission_message() {
         WebElement contactUs_Submission_Message = driver.findElement(By.xpath("//div[@id='contact_reply']/h1"));
         Assert.assertEquals(contactUs_Submission_Message.getText(), "Thank You for your Message!");
+
+
 
 
     }
--- src/test/resources/features/Contact_Us.feature
+++ src/test/resources/features/Contact_Us.feature
@@ -1,6 +1,6 @@
 Feature: WebDriver University - Contact Us Page
 
- Scenario: Validate Successful Submission - Unique Data
+  Scenario: Validate Successful Submission - Unique Data
     Given I access the webdriver university contact us page
     When I enter a unique first name
     And I enter a unique last name
@@ -8,5 +8,15 @@ Feature: WebDriver University - Contact Us Page
     And I enter a unique comment
     And I click on the submit button
     Then I should be presented with a successful contact us submission message
+
+  Scenario: Validate Successful Submission - Specific Data
+    Given I access the webdriver university contact us page
+    When I enter a specific first name Joe
+    And I enter a specific last name Blogs
+    And I enter a specific email address joe_blogs123@mail.com
+    And I enter a specific comment "How are you?"
+    And I click on the submit button
+    Then I should be presented with a successful contact us submission message
+
 
 
