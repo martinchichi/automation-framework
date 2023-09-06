9fcbb5cf70171dc81725b90e8d9c4f859b769a9c
martinchichi <martincicinelli@gmail.com>
2023-09-06 13:08:56 -0300

    Attaching Images

--- src/test/java/runners/MainRunner.java
+++ src/test/java/runners/MainRunner.java
@@ -6,8 +6,8 @@ import org.testng.annotations.DataProvider;
 
 
 @CucumberOptions(features = {"classpath:features"}, glue = {"stepDefinitions"},
-        tags = "@login", monochrome = true, dryRun = false,
-        plugin = {"pretty", "html:target/login.htm", "json:target/login.json"})
+        tags = "@contact-us", monochrome = true, dryRun = false,
+        plugin = {"pretty", "html:target/cucumber.htm", "json:target/cucumber.json"})
 public class MainRunner extends AbstractTestNGCucumberTests {
 /* This method overrides the "scenarios" method and enables parallel test execution
  by using the "@DataProvider" annotation. It provides test data and returns the
--- src/test/java/stepDefinitions/base/Hooks.java
+++ src/test/java/stepDefinitions/base/Hooks.java
@@ -1,7 +1,14 @@
 package stepDefinitions.base;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static driver.DriverFactory.cleanupDriver;
 import static driver.DriverFactory.getDriver;
 
@@ -10,6 +17,23 @@ public class Hooks {
     public void setup() {
         getDriver();
     }
+   @AfterStep
+   public void captureExceptionImage(Scenario scenario){
+       if (scenario.isFailed()){
+           // Captures a screenshot and stores it as a byte array
+           byte[] screenshot = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.BYTES);
+           //Convert timestamp to Date object
+           long timestamp = System.currentTimeMillis();
+           Date date  = new Date(timestamp);
+           //Format date
+           SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+           String formattedTime = dateFormat.format(date);
+           scenario.attach(screenshot,"image/png", "Attached Image- " + formattedTime);
+       }
+
+   }
+
+
 
     @After
     public void tearDown() {
